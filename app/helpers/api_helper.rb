dd2ee0117460bcd7d583dcdd6ec71dd09cd7e1fe
MK <mk@optimalconsulting.com>
2012-10-16 16:21:48 +0530

    Add comment box via JS

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -7,4 +7,20 @@ module ApiHelper
   	raise "'@jsonp' must be set!" if !@jsonp
   	"#{@jsonp}(#{options.to_json})".html_safe
   end
+
+  def juvia_respond(options)
+    "Juvia.handleResponsOffCallback(#{options.to_json})".html_safe
+  end
+
+  def juvia_reinstall_behavior
+    "Juvia.reinstallBehavior();"
+  end
+
+  def avatar_img(author_email, author_email_md5)
+    if author_email
+      return "http://www.gravatar.com/avatar/#{author_email_md5}?s=64"
+    else
+      return "http://www.gravatar.com/avatar/generic?s=64"
+    end
+  end
 end
