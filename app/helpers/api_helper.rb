917ca7a20cb15fd27277c53bb7d74ebf49d1ad2e
MK <mk@optimalconsulting.com>
2012-10-31 21:35:26 +0530

    enable reply if user logged-in and link on user image

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -15,4 +15,19 @@ module ApiHelper
       return "http://www.gravatar.com/avatar/generic?s=64"
     end
   end
+  
+  def comment_hash(comment, username, user_email)
+    return {:comment_counter => 1,
+	  :comment_id => comment.id,
+	  :user_image => avatar_img(comment.author_email, (comment.author_email_md5 rescue '')),
+	  :user_name => comment.author_name,
+	  :comment_text => render_markdown(comment.content),
+	  :creation_date => distance_of_time_in_words(Time.now, comment.created_at) + ' ago',
+	  :comment_votes => comment.total_like,
+	  :liked => (comment.liked?(username, user_email) ? "liked" : "unliked"),
+	  :flagged => (comment.flagged),
+	  :user_email => comment.author_email
+    }
+  end
+  
 end
