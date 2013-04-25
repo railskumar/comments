d4927544c3daef3e35342f0d4ca673eaff177378
MK <mk@optimalconsulting.com>
2013-04-25 18:08:11 +0530

    bring back user comments without exposing user emails

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -9,15 +9,16 @@ class ApiController < ApplicationController
   skip_before_filter :verify_authenticity_token, :authenticate_user!
   before_filter :handle_cors, :populate_variables
   before_filter :check_restrict_comment_length, :only => [ :add_comment, :update_comment ]
+  before_filter :decode_value, :only => [ :user_comments, :append_user_comments ]
  
   def user_comments
     prepare!([:site_key, :username, :user_email, :container],[:html, :js])
-    @comments = Site.get_site(params[:site_key])[0].comments.by_user(params[:username], params[:user_email]).page(1).per(PER_PAGE)
+    @comments = Site.get_site(params[:site_key])[0].comments.by_user(params[:username], @useremail).page(1).per(PER_PAGE)
   end 
 
   def append_user_comments
     prepare!([:site_key, :username, :user_email, :container],[:html, :js])
-    @comments = Site.get_site(params[:site_key])[0].comments.by_user(params[:username], params[:user_email])
+    @comments = Site.get_site(params[:site_key])[0].comments.by_user(params[:username], @useremail)
     .page(params[:page].to_i)
     .per(PER_PAGE)
   end 
@@ -57,6 +58,11 @@ class ApiController < ApplicationController
   end
 
 private
+
+  def decode_value
+    @useremail = decode_str(params[:user_email])
+  end
+
   def log_exception(e)
     logger.error("#{e.class} (#{e}):\n  " <<
       e.backtrace.join("\n  "))
