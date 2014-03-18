f690b132faeb44d925f9b4bdb479eb19f55c3feb
railskumar <ashish.rubyonrails@gmail.com>
2014-03-18 12:34:26 +0530

    add deployment script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,7 @@
-require "bundler/capistrano"
-require "rvm/capistrano"
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"    
+require 'bundler/capistrano'
 require "erb"
-
-default_run_options[:pty] = true
-
-role :web, "50.16.48.4"
-role :app, "50.16.48.4"
-role :db, "50.16.48.4", :primary => true
 
 set(:rvm_type)          { :system }
 set(:rvm_ruby_string)   { "1.9.3-p374" }
@@ -14,21 +9,23 @@ set(:ruby_version)      { '1.9.3-p374' }
 set(:rvm_path)          { "/usr/local/rvm" }
 set :ruby_path, "/usr/local/bin/ruby"
 
-set :user, 'ec2-user'
-set :use_sudo, false
-set :rake, 'bundle exec rake'
-set :deploy_to, "/srv/rails/comments"
-set :rails_env, "production"
-
-set :scm, 'git'
-set :branch, 'master'
+set :scm, :git
 set :repository, "https://github.com/railskumar/comments.git"
-set :git_enable_submodules, 1
-set :git_shallow_clone, 1
-set :scm_verbose, true
+set :application, "comments"
+set :branch, "master"
+set :deploy_to, '/srv/rails/comments'
+set :rails_env, 'production'
+set :use_sudo, false
 
-set :deploy_via, :remote_cache
-set :repository_cache, "cached_copy"
+set :user, 'ec2-user'
+
+#set :git_enable_submodules, 1
+#set :git_shallow_clone, 1
+#set :scm_verbose, true
+
+#set :deploy_via, :remote_cache
+#set :repository_cache, "cached_copy"
+set :deploy_via, :copy
 
 ssh_options[:port] = 22
 ssh_options[:username] = 'ec2-user'
@@ -37,56 +34,36 @@ ssh_options[:paranoid] = false
 ssh_options[:forward_agent] = true
 ssh_options[:keys] = %w(~/ssh-keys/similar/Similar-Beliefs.pem)
 
-after "deploy:setup", "deploy:create_shared_directories"
+default_run_options[:pty] = true
 
-after "deploy:create_symlink", "deploy:link_shared_directories"
+role :web, "54.83.7.118"
+role :app, "54.83.7.118"
+role :db, "54.83.7.118", :primary => true
 
-after "deploy:p_assets", "deploy:restart"
-after "deploy:cp_assets", "deploy:restart"
 
-after "deploy", "deploy:cleanup"
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
 
 namespace :deploy do
+  desc "symlink all files in the shared directory"
+  task :shared_symlink do
+    #run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
   task :start do ; end
   task :stop do ; end
-
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "sudo touch #{File.join(current_path, 'tmp', 'restart.txt')}"
-  end
-
-  task :cp_assets, :roles => :app do
-    run "rm -rfv #{current_path}/public/assets/*; cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
-  end
-
-  task :p_assets, :roles => :app do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
-  end
-
-  task :create_shared_directories, :role => :app do
-    run "mkdir -p #{shared_path}/sockets"
-    run "mkdir -p #{shared_path}/uploads"
-    run "mkdir -p #{shared_path}/pids"
-    run "mkdir -p #{shared_path}/log"
-    run "mkdir -p #{shared_path}/bundle"
-    run "mkdir -p #{shared_path}/assets"
-  end
-
-  task :link_shared_directories, :roles => :app do
-    run "rm -rf #{current_path}/tmp/sockets; ln -s #{shared_path}/sockets #{current_path}/tmp/sockets"
-    run "rm -rf #{current_path}/public/uploads; ln -s #{shared_path}/uploads #{current_path}/public/uploads"
-    run "rm -rf #{current_path}/public/assets; ln -s #{shared_path}/assets #{current_path}/public/assets"
-  end
-
-  task :db_create, :roles => :app do
-    run "cd #{current_path};RAILS_ENV=#{rails_env} bundle exec rake db:create"
-  end
-
-  task :db_migrate, :roles => :app do
-    run "cd #{current_path};RAILS_ENV=#{rails_env} bundle exec rake db:migrate"
-  end
-
-  task :db_seed, :roles => :app do
-    run "cd #{current_path};RAILS_ENV=#{rails_env} bundle exec rake db:seed"
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
 
+namespace :comments do
+  task :symlink do
+    run "ln -nfs #{shared_path}/system/uploads #{release_path}/public/uploads"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+before "deploy:assets:precompile", "deploy:shared_symlink"
+after "deploy:symlink", "comments:symlink"
